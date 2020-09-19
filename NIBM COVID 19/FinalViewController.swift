a02ead5a215c2f56724a7a896b167aaeeafabd9f
Lakinsha <49357214+Lakinsha@users.noreply.github.com>
2020-09-19 02:20:30 -0700

    Create FinalViewController.swift

---
+++ NIBM COVID 19/FinalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FinalViewController.swift
+//  NIBM COVID 19
+//
+//  Created by lakinsha on 9/18/20.
+//  Copyright © 2020 lakinsha. All rights reserved.
+//
+
+import UIKit
+
+class FinalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

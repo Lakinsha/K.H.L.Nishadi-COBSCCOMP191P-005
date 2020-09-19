5af619e2a12bebd7c04e5f7fe56eb5366e99c68e
Lakinsha <49357214+Lakinsha@users.noreply.github.com>
2020-09-19 02:21:17 -0700

    Create NIBM_COVID_19Tests.swift

---
+++ NIBM COVID 19Tests/NIBM_COVID_19Tests.swift
@@ -0,0 +1,34 @@
+//
+//  NIBM_COVID_19Tests.swift
+//  NIBM COVID 19Tests
+//
+//  Created by lakinsha on 9/17/20.
+//  Copyright © 2020 lakinsha. All rights reserved.
+//
+
+import XCTest
+@testable import NIBM_COVID_19
+
+class NIBM_COVID_19Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

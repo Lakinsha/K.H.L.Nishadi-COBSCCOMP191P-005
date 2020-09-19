53afe1a58b692c9252f90fd17d02a479f9bfe6d8
Lakinsha <49357214+Lakinsha@users.noreply.github.com>
2020-09-19 02:20:45 -0700

    Create LoginViewController.swift

---
+++ NIBM COVID 19/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  NIBM COVID 19
+//
+//  Created by lakinsha on 9/17/20.
+//  Copyright © 2020 lakinsha. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBAction func EmailTextField(_ sender: UITextField) {
+    }
+    @IBAction func PasswordTextField(_ sender: UITextField) {
+    }
+    @IBAction func SignInButton(_ sender: UIButton) {
+    }
+    @IBAction func NeedAccountButton(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

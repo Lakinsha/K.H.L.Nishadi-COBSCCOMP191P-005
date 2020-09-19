73a33d7e5b40298c84dcd410b5e9930de96c63bd
Lakinsha <49357214+Lakinsha@users.noreply.github.com>
2020-09-19 02:21:10 -0700

    Create ViewController.swift

---
+++ NIBM COVID 19/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NIBM COVID 19
+//
+//  Created by lakinsha on 9/17/20.
+//  Copyright © 2020 lakinsha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func SignInButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func SignUpButton(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
